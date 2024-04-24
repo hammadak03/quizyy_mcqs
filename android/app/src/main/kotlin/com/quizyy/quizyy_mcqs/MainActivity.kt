385e5f7fa7e71dd2c35eedbc0262436f8a56bfca
Hammad Ahmed <hammadak350@gmail.com>
2024-04-24 13:26:17 +0500

    Uploaded.

---
+++ android/app/src/main/kotlin/com/quizyy/quizyy_mcqs/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.quizyy.quizyy_mcqs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const primaryBackgroundColor = Color(0xff153448);
+const backgroundSecondaryColor = (0xff3C5B6F);
+const buttonPrimaryColor = (0xff948979);
+const textPrimaryColor = (0xffDFD0B8);
+const textFieldColor = (0xff367189);
+const whiteColor = Color(0xffFFFFFF);
